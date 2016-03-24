934763b932ba99fcb42ae846682bf7c94c52f9b4
Kade <keithkade@gmail.com>
2016-03-23 21:57:09 -0500

    failed login now displays message

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,15 +1,16 @@
 class LoginController < ApplicationController
   def index
+    @message = flash[:message]
   end
 
   def create
-    #do validation of credentials
-
+    #todo validation of credentials
     if params['user'] == 'student' && params['password'] == 'root'
       redirect_to '/home'
     elsif params['user'] == 'admin' && params['password'] == 'root'
       redirect_to '/admin/classes'
     else
+      flash[:message] = "Login Failed. Please Try Again"
       redirect_to '/login'
     end
 
