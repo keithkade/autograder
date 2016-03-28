197ab7292653e8281931f44812c067546ae0dc7b
Kade <keithkade@gmail.com>
2016-03-28 11:44:11 -0500

    always use https

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Testapp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # only serve over https since we are sending passwords
+    config.force_ssl = true
   end
 end
