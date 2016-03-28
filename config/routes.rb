b294e1e951d738a3de27d3d4c3c5d0f347d92f0f
bblok <williambrachoblok@gmail.com>
2016-03-28 22:54:34 +0000

    courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :courses
   get 'student/home'
 
   resources :problems
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323211734) do
+ActiveRecord::Schema.define(version: 20160326091640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "problems", force: :cascade do |t|
     t.string   "title"
