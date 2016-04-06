d4493d4ffa4f00ec96d5d87b58b7e2f1be86fe5e
Auroratide <tf.auroratide@gmail.com>
2016-04-06 18:25:58 +0000

    Can now create problem directly for course

--- app/models/course_problem_relation.rb
+++ app/models/course_problem_relation.rb
@@ -1,2 +1,31 @@
 class CourseProblemRelation < ActiveRecord::Base
+  # Use this instead of create!.  This will automatically handle whether the course/problem exist
+    def self.relate!(courseid, problemid)
+        course = Course.find_by_id(courseid)
+        problem = Problem.find_by_id(problemid)
+        if not course.nil? and not problem.nil?
+            @rel = self.create!({ :course => course.id, :problem => problem.id })
+            @rel.save
+            return @rel
+        else
+            return nil
+        end
+    end
+    
+# Could probably be DRYer, but meh
+  # ANYTIME a problem is destroyed, call this also
+    def self.destroy_by_problem(problemid)
+        rels = CourseProblemRelation.where(:problem => problemid)
+        rels.each do |rel|
+            rel.destroy
+        end
+    end
+    
+  # ANYTIME a course is destroyed, call this also
+    def self.destroy_by_course(courseid)
+        rels = CourseProblemRelation.where(:course => courseid)
+        rels.each do |rel|
+            rel.destroy
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402152738) do
+ActiveRecord::Schema.define(version: 20160402154438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160402152738) do
   create_table "course_problem_relations", force: :cascade do |t|
     t.integer  "course"
     t.integer  "problem"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "course_user_relations", force: :cascade do |t|
+    t.integer  "course"
+    t.integer  "user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
