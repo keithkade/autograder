1b9a0e66c9c577e16623290b1fdb87f19f3c6654
Auroratide <tf.auroratide@gmail.com>
2016-04-13 21:29:19 +0000

    submissions and junk

--- app/controllers/admin/problems_controller.rb
+++ app/controllers/admin/problems_controller.rb
@@ -93,7 +93,7 @@ class Admin::ProblemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def problem_params
-      params.require(:problem).permit(:title, :summary, :input_description, :output_description, :skeleton, :due_date)
+      params.require(:problem).permit(:title, :summary, :input_description, :output_description, :skeleton, :due_date, :language)
     end
     
     def relate_with_courses
