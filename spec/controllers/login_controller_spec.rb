ef001160bc84afcec8688791780528aac20c701a
Kade <keithkade@gmail.com>
2016-04-12 21:23:05 -0500

    comment out redundant tests

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -16,15 +16,18 @@ describe LoginController do
       Student.should_receive(:find_by).with({UserName: @student.username, Password: @student.password})
       post :create, { :user => @student.username, :password => @student.password }
     end
-    it 'should login valid students' do
-      controller.should_receive(:log_in_student)
-      post :create, { :user => @student.username, :password => @student.password }
-    end
-    it 'should logout students' do
-      controller.should_receive(:log_out_student)
-      post :create, { :user => @student.username, :password => @student.password }
-      delete :destroy
-    end
+
+    #these were redundant
+    #it 'should login valid students' do
+    #  controller.should_receive(:log_in_student)
+    #  post :create, { :user => @student.username, :password => @student.password }
+    #end
+    #it 'should logout students' do
+    #  controller.should_receive(:log_out_student)
+    #  post :create, { :user => @student.username, :password => @student.password }
+    #  delete :destroy
+    #end
+
   end
 
 end
